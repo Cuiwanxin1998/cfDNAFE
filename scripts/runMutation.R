d31ff69d0c97bfac0fa73c196fe8527d6b046008
Cuiwanxin1998 <38834114+Cuiwanxin1998@users.noreply.github.com>
2023-04-24 10:10:37 +0800

    Update runMutation.R

--- scripts/runMutation.R
+++ scripts/runMutation.R
@@ -1,23 +1,10 @@
-if (!require("BiocManager", quietly = TRUE)){
-  install.packages("BiocManager")
-}
-if (!require("optparse", quietly = TRUE)){
-  install.packages("optparse")
-}
-if (!require("BSgenome.Hsapiens.UCSC.hg38", quietly = TRUE)){
-  BiocManager::install("BSgenome.Hsapiens.UCSC.hg38")
-}
-if (!require("BSgenome.Hsapiens.UCSC.hg19", quietly = TRUE)){
-  BiocManager::install("BSgenome.Hsapiens.UCSC.hg19")
-}
-
 library(optparse)
 
 option_list <- list(
   make_option(c("--refGenome"),type = "character", default="hg38", help = "Path to normal WIG file. Default: [%default]"),
   make_option(c("--inputFile"),type = "character", help = "Path to VCF file; Require"), 
-  make_option(c("--outputfile1"), type='character', default='test', help='96 mutational profile. Default: [%default]')
-  make_option(c("--outputfile2"), type='character', default='test', help='Fit mutation matrix to the COSMIC mutational signatures. Default: [%default]')
+  make_option(c("--outputfile1"), type='character', default='test.96.mutation.profile', help='96 mutational profile. Default: [%default]'),
+  make_option(c("--outputfile2"), type='character', default='test.signatures', help='Fit mutation matrix to the COSMIC mutational signatures. Default: [%default]')
   )
 parseobj <- OptionParser(option_list=option_list)
 opt <- parse_args(parseobj) 
